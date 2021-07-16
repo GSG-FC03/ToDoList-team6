65b655f6ac3b3ed1cf1094ad08839cccf927fec7
mo7amedeahb97 <gazacityface@gmail.com>
2021-07-16 15:07:23 +0300

    declare element variabls using dom relates #18

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// declareing the variabale 
+const inpText = document.getElementById('input-text');
+const btnAdd = document.getElementById('key-add');
+const keyinp = document.getElementById('key-inp');
+const result = document.querySelector(".result");
+const removeBtn = document.getElementById('delete-btn');
+const editContent = document.getElementById('edit-btn');
+
+// declareing the variabale 
