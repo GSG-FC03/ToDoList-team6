a8f4cfa35fd31d326c654532a64b7d5ccf3c92ad
mo7amedeahb97 <gazacityface@gmail.com>
2021-07-17 16:26:43 +0300

    declare the elements to variabals using dom to work on them relates #18

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 // declareing the variabale 
-const inpText = document.getElementById('input-text');
-const btnAdd = document.getElementById('key-add');
-const keyinp = document.getElementById('key-inp');
-const result = document.querySelector(".result");
-const removeBtn = document.getElementById('delete-btn');
-const editContent = document.getElementById('edit-btn');
+const todoInp = document.getElementById("todos");
+const addTodo = document.getElementById("add-todos");
+const getTodo = document.querySelector(".get-result");
+const saveBtn = document.getElementById("save-btn")
+let saveIndex = document.getElementById("save-index")
+let clearAll = document.getElementById("clear-all");
 
 // declareing the variabale 
