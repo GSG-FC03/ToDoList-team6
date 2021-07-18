8fb558ce7ff4f2742b1d1409f8ab38c7e70db267
safamohanna95 <safamohanna95@gmail.com>
2021-07-18 22:16:53 +0300

    How the edit button works: when a user clicks on the edit button, the value and show on the input felid and then the save button will show next to the add button and the add button will get disabled relats #29

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,25 @@ let saveIndex = document.getElementById("save-index")
 let clearAll = document.getElementById("clear-all");
 
 // declareing the variabale 
+
+// How the edit button works: when a user clicks on the edit button, the value and show on the input felid and then the save button will show next to the add button and the add button will get disabled
+function editTodo(i){
+    let toDoTask = localStorage.getItem("localtodo");
+    saveIndex.value = i;
+    let todoObj = JSON.parse(toDoTask);
+    todoInp.value = todoObj[i];
+    saveBtn.style.display = "block";
+    addTodo.setAttribute("disabled", true)
+    };
+
+    saveBtn.addEventListener('click', ()=> {
+        let toDoTask = localStorage.getItem("localtodo");
+        let todoObj = JSON.parse(toDoTask);
+        let savedIndex = saveIndex.value;
+        todoObj[savedIndex] = todoInp.value;
+        localStorage.setItem("localtodo", JSON.stringify(todoObj));
+        showTodo();
+        addTodo.removeAttribute("disabled",false)
+        saveBtn.style.display = "none"; 
+        })
+        // end of the edit function
