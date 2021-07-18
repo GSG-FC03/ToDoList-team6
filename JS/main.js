45e6ad4c6addd4c5d8d5dffa98f7481d337cf177
mo7amedeahb97 <gazacityface@gmail.com>
2021-07-18 15:34:24 +0300

    add eventlistener to the add btn -click- relates #19

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,23 @@ let saveIndex = document.getElementById("save-index")
 let clearAll = document.getElementById("clear-all");
 
 // declareing the variabale 
+// add event lisener to the (add) button 
+// explanation "when the user click on add its take the input value and assign it to array in localstorage withe the same key and every time you clicked add btn and write any value in the input it push it to the same array "
+addTodo.addEventListener('click', () => {
+    let todoInpVal = todoInp.value ;
+    if(todoInpVal.trim()!=0){
+    let toDoTask = localStorage.getItem("localtodo");
+    
+    if(toDoTask == null){
+      todoObj = [];
+    }
+     else{ 
+      todoObj = JSON.parse(toDoTask) ;
+    }
+    todoObj.push(todoInpVal);
+    localStorage.setItem("localtodo", JSON.stringify(todoObj))
+    }
+    location.reload();
+    
+    })
+//end of the  add event lisener to the (add) button
