7826fecdb41d3f4c6676be8ac201f10cba620da4
Mosab-Ahmed <mosab.msameh@gmail.com>
2021-07-18 21:35:09 +0300

    add function for show TO-DOS in result section relate #27

--- JS/main.js
+++ JS/main.js
@@ -6,4 +6,27 @@ const saveBtn = document.getElementById("save-btn")
 let saveIndex = document.getElementById("save-index")
 let clearAll = document.getElementById("clear-all");
 
-// declareing the variabale 
+// declareing the variabale
+// show TO-DOs  
+function showTodo(){
+  let toDoTask = localStorage.getItem("localtodo");
+  toDoTask == null ?todoObj=[]:todoObj = JSON.parse(toDoTask);
+  let finalTodo = '';
+let addTodoList = getTodo;
+todoObj.forEach((item, index) =>{
+  finalTodo +=` 
+  <h3> Key: ${index+1}</h3>  
+  <p> To-do: ${item} </p> 
+ <p> Date:  ${new Date().toLocaleDateString()}</p>
+<div class="icons">
+<i class="fas fa-trash-alt " id="del-icon" onclick="deleteTodo(${index})"></i>
+<i class="far fa-edit" id="edit-icon" onclick="editTodo(${index})"></i>
+
+</div>
+<br>
+`
+});
+getTodo.innerHTML = finalTodo;
+
+}
+// end of show TO-DOS function
